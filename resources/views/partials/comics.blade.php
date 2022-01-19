953cc8e7b508ecbb05f876b74a56ed163a7d6572
Bonu93 <luca.morucci.kb@gmail.com>
2022-01-19 17:33:19 +0100

    add comics

--- resources/views/partials/comics.blade.php
+++ resources/views/partials/comics.blade.php
@@ -1,7 +1,12 @@
 <div class="container">
     <div class="gallery d-flex">
-        <div class="card">
-            
-        </div>
+        @foreach ($comics as $comic)
+            <div class="card d-flex flex-column">
+                <div class="card-img">
+                    <img src="{{$comic['thumb']}}" alt="">
+                </div>
+                <h3>{{$comic['series']}}</h3>
+            </div>
+        @endforeach
     </div>
 </div>
