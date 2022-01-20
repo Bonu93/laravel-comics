4feb51ebfe8697bc34ffa73cb389ec9f8ea3067e
Bonu93 <luca.morucci.kb@gmail.com>
2022-01-20 16:37:14 +0100

    complete

--- resources/views/comic-detail.blade.php
+++ resources/views/comic-detail.blade.php
@@ -3,17 +3,37 @@
 @section('main-content')
 <section class="comic-detail">
     <div class="comic-jumbo">
-        <div class="detail-container">
+        <div class="detail-container h-100">
             <img class="thumb" src="{{$comic['thumb']}}" alt="">
         </div>
     </div>
-    <section class="content">
+    <section class="content d-flex flex-column justify-content-center align-items-center">
         <div class="detail-container d-flex">
             <div class="description">
-                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Non distinctio eaque ullam vel enim. Pariatur voluptatibus qui eligendi voluptas molestiae. Voluptatibus officia deserunt officiis aut iusto ut eligendi sit fugit?</p>
+                <h3>{{$comic['title']}}</h3>
+                <div class="green-bar d-flex align-items-center w100">
+                    <div class="buy-details d-flex justify-content-between">
+                        <div class="price">
+                            <span class="detail">U.S. Price: </span>
+                            <span>{{$comic['price']}}</span>
+                        </div>
+                        <div class="available">
+                            <span class="detail">
+                                AVAILAIBLE
+                            </span>
+                        </div>
+                    </div>
+                    <div class="cta text-center">
+                        <a href="">Check Availability</a>
+                    </div>
+                </div>
+                <p>{{$comic['description'] }}</p>
+                
             </div>
             <div class="ad">
-                <img src="{{asset('images/adv.jpg')}}" alt="">
+                <a href="">
+                    <img src="{{asset('images/adv.jpg')}}" alt="">
+                </a>
             </div>
         </div>
     </section>
