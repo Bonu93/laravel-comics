2df45634e945a0bafb24450be4d9034db33dd14d
Bonu93 <luca.morucci.kb@gmail.com>
2022-01-20 15:54:04 +0100

    add detail page draft

--- resources/views/comic-detail.blade.php
+++ resources/views/comic-detail.blade.php
@@ -1,5 +1,22 @@
 @extends('layouts.main')
 
 @section('main-content')
-<h2>product</h2>
+<section class="comic-detail">
+    <div class="comic-jumbo">
+        <div class="detail-container">
+            <img class="thumb" src="{{$comic['thumb']}}" alt="">
+        </div>
+    </div>
+    <section class="content">
+        <div class="detail-container d-flex">
+            <div class="description">
+                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Non distinctio eaque ullam vel enim. Pariatur voluptatibus qui eligendi voluptas molestiae. Voluptatibus officia deserunt officiis aut iusto ut eligendi sit fugit?</p>
+            </div>
+            <div class="ad">
+                <img src="{{asset('images/adv.jpg')}}" alt="">
+            </div>
+        </div>
+    </section>
+</section>
+
 @endsection
--- resources/views/partials/comics.blade.php
+++ resources/views/partials/comics.blade.php
@@ -1,17 +1,19 @@
-<div class="container">
-    <div class="gallery d-flex justify-content-center">
-        @foreach ($comics as $index => $comic)
-            <div class="card d-flex flex-column">
-                <a href="{{ route('comic-detail', ['id' => $index])}}">
-                    <div class="card-img">
-                        <img src="{{$comic['thumb']}}" alt="">
-                    </div>
-                    <h3>{{$comic['series']}}</h3>
-                </a>
+<section class="comics">
+    <div class="container">
+        <div class="gallery d-flex justify-content-center">
+            @foreach ($comics as $index => $comic)
+                <div class="card d-flex flex-column">
+                    <a href="{{ route('comic-detail', ['id' => $index])}}">
+                        <div class="card-img">
+                            <img src="{{$comic['thumb']}}" alt="">
+                        </div>
+                        <h3>{{$comic['series']}}</h3>
+                    </a>
+                </div>
+            @endforeach
+            <div class="cta">
+                <button class="load-more">Load more</button>
             </div>
-        @endforeach
-        <div class="cta">
-            <button class="load-more">Load more</button>
         </div>
     </div>
-</div>
+</section>
