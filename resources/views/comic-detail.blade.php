39b6f9bf89ec0f142da0d40cc2413472d9663d30
Bonu93 <luca.morucci.kb@gmail.com>
2022-01-20 15:16:49 +0100

    add config and new route

---
+++ resources/views/comic-detail.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.main')
+
+@section('main-content')
+<h2>product</h2>
+@endsection
--- resources/views/partials/comics.blade.php
+++ resources/views/partials/comics.blade.php
@@ -1,11 +1,13 @@
 <div class="container">
     <div class="gallery d-flex justify-content-center">
-        @foreach ($comics as $comic)
+        @foreach ($comics as $index => $comic)
             <div class="card d-flex flex-column">
-                <div class="card-img">
-                    <img src="{{$comic['thumb']}}" alt="">
-                </div>
-                <h3>{{$comic['series']}}</h3>
+                <a href="{{ route('comic-detail', ['id' => $index])}}">
+                    <div class="card-img">
+                        <img src="{{$comic['thumb']}}" alt="">
+                    </div>
+                    <h3>{{$comic['series']}}</h3>
+                </a>
             </div>
         @endforeach
         <div class="cta">
